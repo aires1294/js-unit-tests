fad03b423be0b0452af68eb87b9199d64c4697c9
aires1294 <pedrotja.engenharia@gmail.com>
2022-07-20 00:54:16 -0300

    item 5 FEITO

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  const lista = {
+    name: nome, 
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return lista;
+};
 
 module.exports = createStudent;
